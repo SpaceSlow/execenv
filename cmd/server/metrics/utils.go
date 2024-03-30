b5d44cc142885d3504849c525bcdfe33bdd49501
Igor Ivanov <igor.ivanov.it@mail.ru>
2024-03-30 22:46:34 +0700

    parse metric function is fixed. Also go version set up 1.21.0

--- cmd/server/metrics/utils.go
+++ cmd/server/metrics/utils.go
@@ -3,14 +3,16 @@ package metrics
 import "strings"
 
 func ParseMetricFromURL(url string) (*Metric, error) {
-	metricFields := make([]string, 3)
-
-	metricFields = strings.FieldsFunc(url, func(r rune) bool {
+	metricFields := strings.FieldsFunc(url, func(r rune) bool {
 		return r == '/'
 	})
 
+	if len(metricFields) == 0 {
+		return nil, &IncorrectMetricTypeOrValueError{}
+	}
 	metricType, err := parseMetricType(metricFields[0])
-	if err != nil {
+
+	if err == nil && len(metricFields) == 1 {
 		return nil, &EmptyMetricNameError{}
 	} else if len(metricFields) != 3 || !metricType.isValid() {
 		return nil, &IncorrectMetricTypeOrValueError{}
