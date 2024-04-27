920c68ad7124e3b661cb2f99d839d60afe1bc957
Igor Ivanov <igor.ivanov.it@mail.ru>
2024-04-27 21:05:14 +0700

    added flags and env variables for file storage settings

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/SpaceSlow/execenv/cmd/flags"
 	"github.com/caarlos0/env"
+	"os"
 )
 
 type Config struct {
-	ServerAddr flags.NetAddress `env:"ADDRESS"`
+	ServerAddr    flags.NetAddress `env:"ADDRESS"`
+	StoreInterval int              `env:"STORE_INTERVAL"`
+	StoragePath   string           `env:"FILE_STORAGE_PATH"`
+	NeededRestore bool             `env:"RESTORE"`
 }
 
 func GetConfigWithFlags() (*Config, error) {
@@ -20,6 +25,16 @@ func GetConfigWithFlags() (*Config, error) {
 	if cfg.ServerAddr.String() == "" {
 		cfg.ServerAddr = flagRunAddr
 	}
+	if os.Getenv("STORE_INTERVAL") == "" {
+		cfg.StoreInterval = flagStoreInterval
+	}
+	if cfg.StoragePath == "" {
+		cfg.StoragePath = flagStoragePath
+	}
+	if os.Getenv("RESTORE") == "" {
+		cfg.NeededRestore = flagNeedRestore
+	}
 
+	fmt.Println(cfg)
 	return cfg, nil
 }
--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"flag"
-
 	"github.com/SpaceSlow/execenv/cmd/flags"
 )
 
 var (
-	flagRunAddr flags.NetAddress
+	flagRunAddr       flags.NetAddress
+	flagStoreInterval int
+	flagStoragePath   string
+	flagNeedRestore   bool
 )
 
 func parseFlags() {
 	flagRunAddr = flags.NetAddress{Host: "localhost", Port: 8080}
 	flag.Var(&flagRunAddr, "a", "address and port to run server")
+	flag.IntVar(&flagStoreInterval, "i", 300, "store interval in secs (default 300 sec)")
+	flag.StringVar(&flagStoragePath, "f", "/tmp/metrics-db.json", "file storage path (default /tmp/metrics-db.json")
+	flag.BoolVar(&flagNeedRestore, "r", true, "needed loading saved metrics from file (default true)")
 
 	flag.Parse()
 }
