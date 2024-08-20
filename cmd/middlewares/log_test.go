98c7e44b4ed45822362a12b7523b90f0ab2cf9dc
Igor Ivanov <igor.ivanov.it@mail.ru>
2024-08-20 17:19:13 +0700

    added check method for WithLogging middleware and fixed imports

--- cmd/middlewares/log_test.go
+++ cmd/middlewares/log_test.go
@@ -2,18 +2,24 @@ package middlewares
 
 import (
 	"encoding/json"
-	"github.com/SpaceSlow/execenv/cmd/logger"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/SpaceSlow/execenv/cmd/logger"
 )
 
 func TestWithLogging(t *testing.T) {
+	expectedDuration := time.Second
+	expectedMethod := []string{http.MethodGet, http.MethodPost}[rand.Intn(2)]
+
 	file, err := os.CreateTemp(t.TempDir(), "temp.*.log")
 	require.NoError(t, err)
 	defer file.Close()
@@ -26,14 +32,13 @@ func TestWithLogging(t *testing.T) {
 	}
 	logger.Log, err = cfg.Build()
 
-	expectedDuration := time.Second
 	handler := WithLogging(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(expectedDuration)
 		w.WriteHeader(http.StatusOK)
 		w.Write(nil)
 	}))
 
-	req, err := http.NewRequest(http.MethodPost, "https://example.org", nil)
+	req, err := http.NewRequest(expectedMethod, "https://example.org", nil)
 	require.NoError(t, err)
 	rr := httptest.NewRecorder()
 	handler.ServeHTTP(rr, req)
@@ -57,6 +62,7 @@ func TestWithLogging(t *testing.T) {
 
 	err = json.Unmarshal(data, &logJSON)
 	require.NoError(t, err)
+	assert.Equal(t, expectedMethod, logJSON.Method)
 	assert.Equal(t, 0, logJSON.Size)
 	assert.Equal(t, http.StatusOK, logJSON.Status)
 	assert.GreaterOrEqual(t, logJSON.Duration, expectedDuration)
