1ff80b29e9443113c4a109dfa4bb4aadef9df9cb
Igor Ivanov <igor.ivanov.it@mail.ru>
2024-08-20 21:06:33 +0700

    refactored the code. added test for CheckConnectionHadler

---
+++ cmd/handlers/check_connection_test.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/SpaceSlow/execenv/cmd/storages"
+)
+
+var _ storages.ICheckConnection = (*MockCheckStorage)(nil)
+
+type MockCheckStorage struct {
+	returnCheck bool
+}
+
+func (s *MockCheckStorage) CheckConnection() bool {
+	return s.returnCheck
+}
+
+func TestDBHandler_Ping(t *testing.T) {
+	tests := []struct {
+		name           string
+		storage        storages.ICheckConnection
+		expectedStatus int
+	}{
+		{
+			name:           "CheckConnection() return true",
+			storage:        &MockCheckStorage{returnCheck: true},
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name:           "CheckConnection() return false",
+			storage:        &MockCheckStorage{returnCheck: false},
+			expectedStatus: http.StatusInternalServerError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handler := CheckConnectionHandler{storage: tt.storage}
+			req, err := http.NewRequest("GET", "/ping", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			handler.Ping(rr, req)
+
+			assert.Equal(t, tt.expectedStatus, rr.Code)
+		})
+	}
+}
